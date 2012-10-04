9996ce7127850083baf39331aae76d61cb5e8b56
Athena Avalos <athena.avalos@gmail.com>
2012-10-03 23:38:13 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6285cc29a0ce12935aa9462d65de88e084037ccbd017842cc35fd18882d6d1a17dd019c9e3448283c26a0d4fdcf3412103d233b266f082ae1728956d94d79dcb'
